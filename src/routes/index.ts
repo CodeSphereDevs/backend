dd24fcf4ad7438d41f6331b285a9f7a1ced52ffa
kevsalas <kevin10salas@gmail.com>
2024-05-31 18:28:12 -0600

    solucionar index.ts
    Exisitan dos llamadas router.use( /auth)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,16 @@ import { Router } from "express";
 //Routes
 import AuthRouter from "./auth";
 import ProjectRouter from "./project.route";
+import UsersRouter from "./user"
+
 
 const router = Router();
 
 router.use("/auth", AuthRouter)
 router.use("/projects",ProjectRouter)
+
+
+router.use("/users", UsersRouter);
 
 
 export default router;
