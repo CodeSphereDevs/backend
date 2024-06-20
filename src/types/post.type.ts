64f0622789c1d8520d196e7a8c6e98abbb1df959
kevsalas <kevin10salas@gmail.com>
2024-06-19 19:15:51 -0600

    finalmente merge todoo

--- src/types/post.type.ts
+++ src/types/post.type.ts
@@ -4,14 +4,6 @@ export type Post = {
     id: string;
     title: string;
     author: string;
-<<<<<<< HEAD
-    date: Date;
-    content: string;
-    transcendence: string;
-    image?: string;
-    url?: string;
-=======
     content: string;
     likes: number;
->>>>>>> main
 }
--- src/types/project.type.ts
+++ src/types/project.type.ts
@@ -1,11 +1,4 @@
 export type Project = {
-<<<<<<< HEAD
-    id?: string;
-    name: string;
-    author: string;
-    collaborators: object;
-}
-=======
     id: string;
     name: string;
     details: string;
@@ -17,4 +10,3 @@ export type Project = {
 
 }
 
->>>>>>> main
